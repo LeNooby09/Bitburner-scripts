e9c87f878332d051caedb780ca70a285411dca81
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-21 21:03:54 +0100

    [CHANGE]: fixed value to safe RAM

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 export async function main(ns) {
 	const target = ns.args[0] || ns.getHostname();
 	const files = ["scripts/weaken.js", "scripts/grow.js", "scripts/hack.js", "main.js"];
-	const threads = Math.floor(ns.getServerMaxRam(target) / ns.getScriptRam("scripts/weaken.js"));
+	const threads = Math.floor(ns.getServerMaxRam(target) / 1.75 );
 
 	let cycle = 0;
 	let neighbor = ns.scan(target).filter(hostname => hostname !== "home");
