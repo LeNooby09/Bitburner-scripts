b62938f0b445c1e8b25c913b90cb653067b26700
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-21 17:46:35 +0100

    refactor

--- main.js
+++ main.js
@@ -2,44 +2,29 @@
 export async function main(ns) {
 	const target = ns.args[0] || ns.getHostname();
 	const files = ["scripts/weaken.js", "scripts/grow.js", "scripts/hack.js", "main.js"];
-	let cycle = 0;
-	let neighbor;
-
-	ns.scp(files, target);
 	const threads = Math.floor(ns.getServerMaxRam(target) / ns.getScriptRam("scripts/weaken.js"));
-	neighbor = ns.scan(target).filter(hostname => hostname !== "home");
-	if (ns.hasRootAccess(target) === false) {
-		ns.exec("scripts/getRoot.js", "home", "1", target);
+
+	let neighbor = ns.scan(target).filter(hostname => hostname !== "home");
+	let Time
+	{
+		let w = ns.getWeakenTime(target) + 5;
+		let h = ns.getHackTime(target) + 5;
+		let g = ns.getGrowTime(target) + 5;
 	}
 
-	while (true) {
-		ns.print("INFO ","---------- cycle: " + cycle + " -----------------");
-		cycle ++;
+	ns.scp(files, target);
 
-		let weakenTime = ns.getWeakenTime(target) + 5;
-		let hackTime = ns.getHackTime(target) + 5;
-		let growTime = ns.getGrowTime(target) + 5;
-
-		if (ns.args[1] === "--replicate" && neighbor.length !== 0) {
-			if (ns.isRunning("main.js", target, neighbor[0])) {
-				neighbor.shift();
+	while(True) {
+		if(ns.getHackingLevel() < getServerRequiredHackingLevel(target) && neighbor.length > 0) {
+			ns.exec("main.js", home, 1, neighbor.shift());
+		} else {
+			if (ns.getServerMoneyAvailable(target) < ns.getServerMaxMoney(target) * 0.9) {
+				await ns.exec("scripts/grow.js", target, threads);
+			} else if (ns.getServerSecurityLevel(target) > 0.2) {
+				await ns.exec("scripts/weaken.js", target, threads);
+			} else {
+				await ns.exec("scripts/hack.js", target, threads);
 			}
-			else {
-				ns.exec("main.js", target, "1", neighbor[0]);
-				neighbor.shift();
-			}
-		}
-		if (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target) + 5) {
-			ns.exec("scripts/weaken.js", target, threads, target);
-			await ns.sleep(weakenTime);
-		}
-		else if (ns.getServerMoneyAvailable(target) < ns.getServerMaxMoney(target) * 0.9) {
-			ns.exec("scripts/grow.js", target, threads, target);
-			await ns.sleep(growTime);
-		}
-		else {
-			ns.exec("scripts/hack.js", target, threads, target);
-			await ns.sleep(hackTime);
 		}
 	}
 }
