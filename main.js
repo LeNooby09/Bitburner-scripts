2370cc47106459129c7c9a8d27bfb196c0adfb7c
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-22 17:15:22 +0100

    [REMOVED]: check root

--- main.js
+++ main.js
@@ -24,19 +24,15 @@ export async function main(ns) {
 	while (true) {
 		ns.print("INFO ", " ---------------- cycle: " + cycle + " -------------------");
 		cycle ++;
-		if (ns.hasRootAccess(target)) {
-			if (ns.getServerMoneyAvailable(dynamicTarget) < ns.getServerMaxMoney(dynamicTarget) * 0.9) {
-				ns.exec("scripts/grow.js", target, threads, dynamicTarget);
-				await ns.sleep(time.g);
-			} else if (ns.getServerSecurityLevel(dynamicTarget) > ns.getServerMinSecurityLevel(dynamicTarget) * 1.1) {
-				ns.exec("scripts/weaken.js", target, threads, dynamicTarget);
-				await ns.sleep(time.w);
-			} else {
-				ns.exec("scripts/hack.js", target, threads, dynamicTarget);
-				await ns.sleep(time.h);
-			}
+		if (ns.getServerMoneyAvailable(dynamicTarget) < ns.getServerMaxMoney(dynamicTarget) * 0.9) {
+			ns.exec("scripts/grow.js", target, threads, dynamicTarget);
+			await ns.sleep(time.g);
+		} else if (ns.getServerSecurityLevel(dynamicTarget) > ns.getServerMinSecurityLevel(dynamicTarget) * 1.1) {
+			ns.exec("scripts/weaken.js", target, threads, dynamicTarget);
+			await ns.sleep(time.w);
 		} else {
-			throw new Error("No root access to " + target);
+			ns.exec("scripts/hack.js", target, threads, dynamicTarget);
+			await ns.sleep(time.h);
 		}
 	}
 }
