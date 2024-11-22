9ef2e4c25af560e222d512e9b7f7b2cc202c878f
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-22 22:52:53 +0100

    [FEATURE]: check if program exists

--- scripts/getRoot.js
+++ scripts/getRoot.js
@@ -1,10 +1,10 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  ns.run("scripts/rootScripts/ftp.js", "1", ns.args[0]);
-  ns.run("scripts/rootScripts/ssh.js", "1", ns.args[0]);
-  ns.run("scripts/rootScripts/sql.js", "1", ns.args[0]);
-  ns.run("scripts/rootScripts/http.js", "1", ns.args[0]);
-  ns.run("scripts/rootScripts/smtp.js", "1", ns.args[0]);
+  if (ns.fileExists("ftpcrack.exe")) { ns.run("scripts/rootScripts/ftp.js", "1", ns.args[0]) }
+  if (ns.fileExists("brutessh.exe")) { ns.run("scripts/rootScripts/ssh.js", "1", ns.args[0]) }
+  if (ns.fileExists("sqlinject.exe")) { ns.run("scripts/rootScripts/sql.js", "1", ns.args[0]) }
+  if (ns.fileExists("httpworm.exe")) { ns.run("scripts/rootScripts/http.js", "1", ns.args[0]) }
+  if (ns.fileExists("relaysmtp.exe")) { ns.run("scripts/rootScripts/smtp.js", "1", ns.args[0]) }
   ns.sleep(5000);
   await ns.nuke(ns.args[0]);
 }
