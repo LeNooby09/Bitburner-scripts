b294d2c14a2f0e251d6d1f9551248cb2a8d71c70
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-23 15:11:02 +0100

    [FEATURE]: install on all servers

--- scripts/install.js
+++ scripts/install.js
@@ -1,5 +1,9 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	const files = ["scripts/weaken.js", "scripts/grow.js", "scripts/hack.js", "main.js"];
-	ns.scp(files, ns.args[0]);
+	let target = ns.args[0]
+	if (ns.args[0] === servers) {
+		target = ns.getPurchasedServers()
+	}
+	ns.scp(files, target);
 }
