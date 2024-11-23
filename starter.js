5e373d41fd16ea302da001b0a52407e01a9252d1
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-23 14:29:26 +0100

    [FIX]: start offset

--- starter.js
+++ starter.js
@@ -4,5 +4,8 @@ export async function main(ns) {
     this is a template kickstarter script
     this script expects your home to have at least 128 GB of ram for all scripts
   */
-  ns.getPurchasedServers().forEach(server => { ns.exec("main.js", "home", "1", server, ns.args[0]) });
+  for (const server of ns.getPurchasedServers()) {
+    ns.exec("main.js", "home", "1", server, ns.args[0]);
+    await ns.sleep(30000);
+  }
 }
