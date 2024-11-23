33df5a9a1ff7c4547f605ede603728dad0683d31
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-23 14:38:01 +0100

    [FIX]: ram count

--- starter.js
+++ starter.js
@@ -2,7 +2,7 @@
 export async function main(ns) {
   /*
     this is a template kickstarter script
-    this script expects your home to have at least 128 GB of ram for all scripts
+    this script expects your home to have at least 95.15 GB of ram for all scripts
   */
   for (const server of ns.getPurchasedServers()) {
     ns.exec("main.js", "home", "1", server, ns.args[0]);
