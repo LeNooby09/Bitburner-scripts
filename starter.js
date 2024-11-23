c0e02f8d1238cfba7d43c811b13705d92f845443
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-23 14:11:55 +0100

    [UPDATE]: template kickstart

--- starter.js
+++ starter.js
@@ -1,17 +1,57 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  ns.exec("main.js","home", "1", "n00dles");
-  ns.exec("main.js","home", "1", "foodnstuff");
-  ns.exec("main.js","home", "1", "sigma-cosmetics");
-  ns.exec("main.js","home", "1", "hong-fang-tea");
-  ns.exec("main.js","home", "1", "harakiri-sushi");
-  ns.exec("main.js","home", "1", "iron-gym");
-  ns.exec("main.js","home", "1", "joesguns");
-  ns.exec("main.js","home", "1", "silver-helix");
-  ns.exec("main.js","home", "1", "omega-net");
-  ns.exec("main.js","home", "1", "neo-net");
-  ns.exec("main.js","home", "1", "phantasy");
-  ns.exec("main.js","home", "1", "zer0");
-  ns.exec("main.js","home", "1", "max-hardware");
-  ns.exec("main.js","home", "1", "crush-fitness");
+  /*
+    this is a template kickstarter script
+    this script expects your home to have at least 128 GB of ram for all scripts
+    it also expects you to have all 25 servers with the following naming scheme "01"..."25"
+  */
+  ns.exec("main.js","home", "1", "01", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "02", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "03", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "04", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "05", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "06", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "07", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "08", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "09", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "10", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "11", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "12", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "13", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "14", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "15", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "16", ns.args[0]);
+  await ns.sleep(1000);
+  ns.exec("main.js","home", "1", "17", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "18", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "19", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "20", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "21", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "22", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "23", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "24", ns.args[0]);
+  await ns.sleep(10000);
+  ns.exec("main.js","home", "1", "25", ns.args[0]);
 }
