f0601c7373f84cda477b7332902258c64f65bb08
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-23 15:07:37 +0100

    [SYNTAX]: usage example

--- scripts/server.js
+++ scripts/server.js
@@ -1,6 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	for (let i = 0; i < ns.args[0]; ++i) {
+	// syntax: scripts/server.js <number of servers to purchase> <ram> <name>
+	for (let i = 0o0; i < ns.args[0]; ++i) {
 		ns.purchaseServer(ns.args[2] + i, ns.args[1]);
 	}
 }
