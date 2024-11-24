b01884fc8edb80697c390ddd7ed43e7ea1fbd664
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-24 17:14:43 +0100

    [FEATURE]: installer installs the installer

--- scripts/install.js
+++ scripts/install.js
@@ -1,6 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	const files = ["scripts/weaken.js", "scripts/grow.js", "scripts/hack.js", "main.js"];
+	const files = ["scripts/weaken.js", "scripts/grow.js", "scripts/hack.js", "main.js", "scripts/install.js"];
 	let target = ns.args[0]
 	if (ns.args[0] === "servers") {
 		ns.getPurchasedServers().forEach(server => {
