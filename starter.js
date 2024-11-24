b62633e3953b109361ca7f6b6fe5d762422ae511
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-24 15:58:32 +0100

    [FIX]: better timing

--- starter.js
+++ starter.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
   let target = ns.args[0] || ns.getPortHandle(1).peek();
-  let avgTime = (ns.getWeakenTime(target) + ns.getGrowTime(target) + ns.getHackTime(target)) / 3;
+  let avgTime = ((ns.getWeakenTime(target) + ns.getGrowTime(target) + ns.getHackTime(target)) / 3) / ns.getPurchasedServers().length;
   for (const server of ns.getPurchasedServers()) {
     ns.exec("main.js", "home", "1", server, target);
     await ns.sleep(avgTime);
