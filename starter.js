7c47335cbd729302d02327b5c7b3e0ee2f83a6ce
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-24 01:11:10 +0100

    [FEATURE]: dynamic optimal timing

--- starter.js
+++ starter.js
@@ -1,11 +1,9 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  /*
-    this is a template kickstarter script
-    this script expects your home to have at least 95.15 GB of ram for all scripts
-  */
+  let target = ns.args[0] || ns.getPortHandle(1).peek();
+  let avgTime = (ns.getWeakenTime(target) + ns.getGrowTime(target) + ns.getHackTime(target)) / 3;
   for (const server of ns.getPurchasedServers()) {
-    ns.exec("main.js", "home", "1", server, ns.args[0]);
-    await ns.sleep(10000);
+    ns.exec("main.js", "home", "1", server, target);
+    await ns.sleep(avgTime);
   }
 }
