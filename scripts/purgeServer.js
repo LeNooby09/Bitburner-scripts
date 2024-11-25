7ebc1052a0a3d208ba14f6a59242994739efcaeb
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-25 08:48:37 +0100

    [FEATURE]: custom amount to purge

--- scripts/purgeServer.js
+++ scripts/purgeServer.js
@@ -1,9 +1,13 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	ns.getPurchasedServers().forEach(server => {
-		if (ns.args[0] === "force") {
-			ns.killall(server);
+	if (ns.args[0] > 0 && Number.isInteger(ns.args[0])) {
+		for (let i = ns.args[0]; i > 0; i--) {
+			for (let i = ns.getPurchasedServers().length; i > 0; i--) {
+				if (ns.args[0 || 1] === "force") {
+					ns.killall(ns.getPurchasedServers()[0]);
+				}
+				ns.deleteServer(ns.getPurchasedServers()[0]);
+			}
 		}
-		ns.deleteServer(server);
-	});
+	}
 }
