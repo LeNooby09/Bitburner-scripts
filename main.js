9f6b8c95045fd90f1779e49b957b467c82adfb5d
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-25 14:44:24 +0100

    [FEATURE]: waiting for to be free instead of crashing

--- main.js
+++ main.js
@@ -14,25 +14,28 @@ export async function main(ns) {
 	}
 
 	while (true) {
-		ns.print("INFO ", " --------------- cycle: " + cycle + " ------------------");
-		cycle ++;
-
 		if (!port.empty()) {
 			dynamicTarget = port.peek();
 			ns.print("INFO ", "Switching to target: " + dynamicTarget);
 		}
-
-		if (ns.getServerSecurityLevel(dynamicTarget) <= ns.getServerMinSecurityLevel(dynamicTarget) + 5) {
-			if (ns.getServerMoneyAvailable(dynamicTarget) < ns.getServerMaxMoney(dynamicTarget) * 0.9) {
-				ns.exec("scripts/grow.js", target, threads, dynamicTarget);
-				await ns.sleep(ns.getGrowTime(dynamicTarget) + 50);
+		if (ns.getServerUsedRam(target) === 0) {
+			ns.print("INFO ", " --------------- cycle: " + cycle + " ------------------");
+			cycle ++;
+			if (ns.getServerSecurityLevel(dynamicTarget) <= ns.getServerMinSecurityLevel(dynamicTarget) + 5) {
+				if (ns.getServerMoneyAvailable(dynamicTarget) < ns.getServerMaxMoney(dynamicTarget) * 0.9) {
+					ns.exec("scripts/grow.js", target, threads, dynamicTarget);
+					await ns.sleep(ns.getGrowTime(dynamicTarget) + 50);
+				} else {
+					ns.exec("scripts/hack.js", target, threads, dynamicTarget);
+					await ns.sleep(ns.getHackTime(dynamicTarget) + 50);
+				}
 			} else {
-				ns.exec("scripts/hack.js", target, threads, dynamicTarget);
-				await ns.sleep(ns.getHackTime(dynamicTarget) + 50);
+				ns.exec("scripts/weaken.js", target, threads, dynamicTarget);
+				await ns.sleep(ns.getWeakenTime(dynamicTarget) + 50);
 			}
 		} else {
-			ns.exec("scripts/weaken.js", target, threads, dynamicTarget);
-			await ns.sleep(ns.getWeakenTime(dynamicTarget) + 50);
+			ns.print("INFO ", "Server " + target + " is not free. Skipping.");
+			await ns.sleep((ns.getGrowTime(target) + ns.getHackTime(target) + ns.getWeakenTime(target)) / 3);
 		}
 	}
 }
