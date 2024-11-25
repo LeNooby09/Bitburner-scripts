500e8dc1d61088c0921d33c3e2691aaa6679615f
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-25 23:36:29 +0100

    [SYNTAX]: minor changes

--- main.js
+++ main.js
@@ -3,39 +3,39 @@ export async function main(ns) {
 	const target = ns.args[0] || ns.getHostname();
 	const threads = Math.floor((ns.getServerMaxRam(target) - ns.getServerUsedRam(target)) / 1.75 );
 
-	let cycle = 0;
+	const buffer = 50;
+	const upperThreshold = 0.9;
+	const lowerThreshold = 0.7;
+	const tolerance = 5;
+
 	let port = ns.getPortHandle(1);
-	let dynamicTarget;
+	let dynamicTarget = ns.args[0] || target;
 	let printedDebug = false;
 
-	if (ns.args[1]) {
-		dynamicTarget = ns.args[1];
-	} else {
-		dynamicTarget = target;
-	}
+	let cycle = 0;
 
 	while (true) {
 		if (!port.empty()) {
 			dynamicTarget = port.peek();
 			if (dynamicTarget !== target) {
-				ns.print("INFO ", "Switching to target: " + dynamicTarget);
+				ns.print("INFO ", "New remote target detected. Switching to: " + dynamicTarget);
 			}
 		}
-		if (ns.getServerUsedRam(target) < ns.getServerMaxRam(target) * 0.9) {
+		if (ns.getServerUsedRam(target) < ns.getServerMaxRam(target) * lowerThreshold) {
 			printedDebug = false;
 			ns.print("INFO ", " --------------- cycle: " + cycle + " ------------------");
 			cycle ++;
-			if (ns.getServerSecurityLevel(dynamicTarget) <= ns.getServerMinSecurityLevel(dynamicTarget) + 5) {
-				if (ns.getServerMoneyAvailable(dynamicTarget) < ns.getServerMaxMoney(dynamicTarget) * 0.9) {
+			if (ns.getServerSecurityLevel(dynamicTarget) <= ns.getServerMinSecurityLevel(dynamicTarget) + tolerance) {
+				if (ns.getServerMoneyAvailable(dynamicTarget) < ns.getServerMaxMoney(dynamicTarget) * upperThreshold) {
 					ns.exec("scripts/grow.js", target, threads, dynamicTarget);
-					await ns.sleep(ns.getGrowTime(dynamicTarget) + 50);
+					await ns.sleep(ns.getGrowTime(dynamicTarget) + buffer);
 				} else {
 					ns.exec("scripts/hack.js", target, threads, dynamicTarget);
-					await ns.sleep(ns.getHackTime(dynamicTarget) + 50);
+					await ns.sleep(ns.getHackTime(dynamicTarget) + buffer);
 				}
 			} else {
 				ns.exec("scripts/weaken.js", target, threads, dynamicTarget);
-				await ns.sleep(ns.getWeakenTime(dynamicTarget) + 50);
+				await ns.sleep(ns.getWeakenTime(dynamicTarget) + buffer);
 			}
 		} else {
 			if (!printedDebug) {
